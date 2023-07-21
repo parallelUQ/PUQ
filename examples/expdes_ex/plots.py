2aa925627082969f73b298750aade36662cb1db4
ozgesurer <ozgesurer2019@u.northwestern.edu>
2023-07-21 12:21:36 -0400

    plots

--- examples/expdes_ex/plots.py
+++ examples/expdes_ex/plots.py
@@ -1,107 +1,53 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 13 14:18:15 2023
-
-@author: ozgesurer
-"""
-import matplotlib.pyplot as plt
 import numpy as np
-from PUQ.surrogate import emulator
-import scipy.stats as sps
+import matplotlib.pyplot as plt 
 
-def plot_EIVAR(xt, cls_data, ninit):
-    
-    plt.scatter(cls_data.x, np.repeat(cls_data.true_theta, len(cls_data.x)), marker='o', color='black')
-    plt.scatter(xt[0:ninit, 0], xt[0:ninit, 1], marker='*', color='blue')
-    plt.scatter(xt[:, 0][ninit:], xt[:, 1][ninit:], marker='+', color='red')
-    plt.axhline(y = cls_data.true_theta, color = 'green')
-    plt.xlabel('x')
-    plt.ylabel(r'$\theta$')
-    plt.show()
-    
-    plt.hist(xt[:, 1][ninit:])
-    plt.axvline(x = cls_data.true_theta, color = 'r')
-    plt.xlabel(r'$\theta$')
-    plt.show()
-    
-    plt.hist(xt[:, 0][ninit:])
-    plt.xlabel(r'x')
-    plt.xlim(0, 1)
-    plt.show()
-    
-def plot_LHS(xt, cls_data):
-    plt.scatter(cls_data.x, np.repeat(cls_data.true_theta, len(cls_data.x)), marker='o', color='black')
-    plt.scatter(xt[:, 0], xt[:, 1], marker='*', color='blue')
-    plt.axhline(y = cls_data.true_theta, color = 'green')
-    plt.xlabel('x')
-    plt.ylabel(r'$\theta$')
-    plt.show()
+def plotline(df, methods, rep_no, w=2, b=1, s='banana', ylim=[0.000001, 1], idstart=0):
 
-def obsdata(cls_data):
-    th_vec = [0.3, 0.4, 0.5, 0.6, 0.7]
-    x_vec  = (np.arange(0, 100, 1)/100)[:, None]
-    fvec   = np.zeros((len(th_vec), len(x_vec)))
-    colors = ['blue', 'orange', 'green', 'red', 'purple']
-    for t_id, t in enumerate(th_vec):
-        for x_id, x in enumerate(x_vec):
-            fvec[t_id, x_id] = cls_data.function(x, t)
-        plt.plot(x_vec, fvec[t_id, :], label=r'$\theta=$' + str(t), color=colors[t_id]) 
+    colors = ['red', 'green', 'blue', 'cyan', 'magenta']
+    markers = ['o', '+', '*', 'D', 'v']
+    linestyles = [(0, ()), (0, (1, 1)), (0, (5, 1)), (0, (3, 5, 1, 5)), (0, (5, 10))]
+    fig, ax = plt.subplots()
 
-    for d_id, d in enumerate(cls_data.des):
-        for r in range(d['rep']):
-            plt.scatter(d['x'], d['feval'][r], color='black')
-    plt.xlabel('x')
-    plt.legend()
-    plt.show()
-    
-def create_test(cls_data):
-    thetamesh   = np.linspace(cls_data.thetalimits[1][0], cls_data.thetalimits[1][1], 100)
-    xdesign_vec = np.tile(cls_data.x.flatten(), len(thetamesh))
-    thetatest   = np.concatenate((xdesign_vec[:, None], np.repeat(thetamesh, len(cls_data.x))[:, None]), axis=1)
-    ftest       = np.zeros(len(thetatest))
-    for t_id, t in enumerate(thetatest):
-        ftest[t_id] = cls_data.function(thetatest[t_id, 0], thetatest[t_id, 1])
+    idmse = 210
+    maxval = 0
+    minval = 1
+    ft = 20
+    for m_id, m in enumerate(methods):
+        dfnnew = df[df['methods'] == m]
+        dfnnnew = dfnnew[dfnnew['theta_id'] <= idmse]
+        
+        mslist = []
+        sdlist = []
+        for i in range(idmse+1):
+            mn = dfnnnew[dfnnnew['theta_id'] == i]['TV'].mean()
+            sd = dfnnnew[dfnnnew['theta_id'] == i]['TV'].std()
+            mslist.append(mn)
+            sdlist.append(1.96*sd/np.sqrt(rep_no))
 
-    ptest = np.zeros(thetamesh.shape[0])
-    ftest = ftest.reshape(len(thetamesh), len(cls_data.x))
-    for i in range(ftest.shape[0]):
-        mean     = ftest[i, :] 
-        rnd      = sps.multivariate_normal(mean=mean, cov=cls_data.obsvar)
-        ptest[i] = rnd.pdf(cls_data.real_data)
-         
-    plt.plot(thetamesh, ptest)
-    plt.show()
-    
-    return thetatest, ftest, ptest, thetamesh 
-    
-def fitemu(xt, f, xt_test, thetamesh, cls_data):
-    x_emu      = np.arange(0, 1)[:, None ]
-    emu = emulator(x_emu, 
-                   xt, 
-                   f, 
-                   method='PCGPexp')
-    from PUQ.surrogatemethods.PCGPexp import  postpred
-    pmeanhat, pvarhat = postpred(emu._info, cls_data.x, xt_test, cls_data.real_data, cls_data.obsvar)
-    
 
-    #emupredict     = emu.predict(x=x_emu, theta=xt_test)
-    #emumean        = emupredict.mean()
-    #emumean = emumean.reshape(len(thetamesh), len(cls_data.x))
-    
-    #posttesthat_c = np.zeros(len(thetamesh))
-    #for i in range(emumean.shape[0]):
-    #    mean = emumean[i, :] 
-    #    rnd = sps.multivariate_normal(mean=mean, cov=cls_data.obsvar)
-    #    posttesthat_c[i] = rnd.pdf(cls_data.real_data)
+        ax.plot(range(0, len(mslist)), mslist,
+                   marker=markers[m_id], 
+                   color=colors[m_id], 
+                   ls=linestyles[m_id], 
+                   markersize=10,
+                   markevery=40,
+                   linewidth=2,
+                   label=m)
 
-    #print(np.round(posttesthat_c - posttesthat, 4))
-    return pmeanhat, pvarhat
-
-def gather_data(xt, cls_data):
-    f    = np.zeros(len(xt))
-    for t_id, t in enumerate(xt):
-        f[t_id] = cls_data.function(xt[t_id, 0], xt[t_id, 1])
+        maxval = max(maxval, max(mslist))
+        minval = min(minval, min(mslist))
+  
+    ax.set_ylim((minval, maxval))
+    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.55), ncol=2, prop={'size': 16})      # 16for small      
+    ax.set_yscale('log') 
+    ax.set_xlabel('Stage', fontsize=ft)
+    ax.set_ylabel('MAD', fontsize=ft)
+    ax.tick_params(axis='both', labelsize=ft)
+    plt.savefig('Figure6_' + s + '.png', bbox_inches='tight')
     
-    return f
-    
+    import os
+    if os.path.exists("ensemble.log"):
+        os.remove("ensemble.log")
+    if os.path.exists("libE_stats.txt"):
+        os.remove("libE_stats.txt")
+        
