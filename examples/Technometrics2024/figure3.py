27983f552790e95f514a33b8d624fef5dcb8de78
ozgesurer <ozgesurer2019@u.northwestern.edu>
2024-10-05 00:03:26 -0400

    Fixing figure3 script and having separate fresco folder

--- examples/Technometrics2024/figure3.py
+++ examples/Technometrics2024/figure3.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PUQ.surrogate import emulator
 from PUQ.posterior import posterior
+from PUQ.designmethods.gen_funcs.acquisition_funcs_support import (
+    get_emuvar,
+    compute_eivar_fig,
+)
 
 
 class sinlinear:
@@ -39,89 +43,98 @@ if __name__ == "__main__":
     # Generate training data
     theta = np.array([-10, -8.7, -7.5, -5, -2.5, -1, 1, 2.5, 5, 7.5, 8.7, 10])[:, None]
     f = cls_sinlin.function(theta)
-    ptr = sps.norm.pdf(cls_sinlin.real_data, f, np.sqrt(cls_sinlin.obsvar))
 
-    # Fit an emulator
-    emu = emulator(cls_sinlin.x, theta, f, method="PCGP")
+    for i in range(3):
+        # Fit an emulator
+        emu = emulator(cls_sinlin.x, theta, f, method="PCGP")
 
-    post = posterior(data_cls=cls_sinlin, emulator=emu)
+        post = posterior(data_cls=cls_sinlin, emulator=emu)
 
-    # Generate test data
-    thetatest = np.arange(-10, 10, 0.0025)[:, None]
-    ftest = cls_sinlin.function(thetatest)
-    ptest = sps.norm.pdf(cls_sinlin.real_data, ftest, np.sqrt(cls_sinlin.obsvar))
+        # Generate test data
+        thetatest = np.arange(-10, 10, 0.0025)[:, None]
+        ftest = cls_sinlin.function(thetatest)
+        ptest = sps.norm.pdf(cls_sinlin.real_data, ftest, np.sqrt(cls_sinlin.obsvar))
 
-    # Predict via the emulator
-    emupred_test = emu.predict(x=cls_sinlin.x, theta=thetatest)
-    emupred_tr = emu.predict(x=cls_sinlin.x, theta=theta)
+        # Predict via the emulator
+        emupred_test = emu.predict(x=cls_sinlin.x, theta=thetatest)
+        emupred_tr = emu.predict(x=cls_sinlin.x, theta=theta)
 
-    posttesthat, posttestvar = post.predict(thetatest)
-    posttrhat, posttrvar = post.predict(theta)
+        posttesthat, posttestvar = post.predict(thetatest)
+        posttrhat, posttrvar = post.predict(theta)
 
-    # Figure 2 (a)
-    ft = 20
-    fig, ax = plt.subplots()
-    ax.plot(thetatest, ftest, color="black")
-    ax.plot(
-        thetatest.flatten(),
-        emupred_test.mean().flatten(),
-        linestyle="dashed",
-        color="blue",
-        linewidth=2.5,
-    )
-    plt.fill_between(
-        thetatest.flatten(),
-        (emupred_test.mean() - np.sqrt(emupred_test.var())).flatten(),
-        (emupred_test.mean() + np.sqrt(emupred_test.var())).flatten(),
-        alpha=0.3,
-    )
-    ax.scatter(theta.T, emupred_tr.mean(), color="red", s=60)
-    ax.set_xlabel(r"$\theta$", fontsize=ft)
-    ax.set_ylabel(r"$\eta(\theta)$", fontsize=ft)
-    ax.tick_params(axis="both", labelsize=ft)
-    plt.savefig("Figure2a.png", bbox_inches="tight")
+        clist = np.arange(-10, 10.01, 0.1)[:, None]
 
-    # Figure 2 (b)
-    fig, ax = plt.subplots()
-    ax.plot(thetatest, ptest, color="black")
-    ax.plot(thetatest, posttesthat, color="blue", linestyle="dashed", linewidth=2.5)
-    plt.fill_between(
-        thetatest.flatten(),
-        (posttesthat - np.sqrt(posttestvar)).flatten(),
-        (posttesthat + np.sqrt(posttestvar)).flatten(),
-        alpha=0.3,
-    )
-    ax.scatter(theta.T, posttrhat, color="red", s=60)
-    ax.set_xlabel(r"$\theta$", fontsize=ft)
-    ax.set_ylabel(r"$p(y|\theta)$", fontsize=ft)
-    ax.tick_params(axis="both", labelsize=ft)
-    plt.savefig("Figure2b.png", bbox_inches="tight")
+        def compute_eivar_full(clist, theta, theta_test, emu):
+            real_x = cls_sinlin.real_x
+            obs = cls_sinlin.real_data
+            obsvar = cls_sinlin.obsvar
+            obsvar3d = obsvar.reshape(1, 1, 1)
+            d_real = real_x.shape[0]
+            x = cls_sinlin.x
+            emupred_test = emu.predict(x=cls_sinlin.x, theta=theta_test)
+            emumean = emupred_test.mean()
+            emuvar, is_cov = get_emuvar(emupred_test)
+            emumeanT = emumean.T
+            emuvarT = emuvar.transpose(1, 0, 2)
+            var_obsvar1 = emuvarT + obsvar3d
+            var_obsvar2 = emuvarT + 0.5 * obsvar3d
+            diags = np.diag(obsvar[real_x, real_x.T])
+            coef = (2**d_real) * (np.sqrt(np.pi) ** d_real) * np.sqrt(np.prod(diags))
 
-    # Figure 2 (c)
-    # Fit an emulator for posterior
-    emu = emulator(cls_sinlin.x, theta, ptr, method="PCGP")
+            # Get the n_ref x d x d x n_cand phi matrix
+            emuphi4d = emu.acquisition(x=x, theta1=theta_test, theta2=clist)
+            acq_func = []
 
-    emupred_test = emu.predict(x=cls_sinlin.x, theta=thetatest)
-    emumean_test = emupred_test.mean()
-    emumean_var = emupred_test.var()
+            # Pass over all the candidates
+            for c_id in range(len(clist)):
+                posteivar = compute_eivar_fig(
+                    obsvar,
+                    var_obsvar2[:, real_x, real_x.T],
+                    var_obsvar1[:, real_x, real_x.T],
+                    emuphi4d[:, real_x, real_x.T, c_id],
+                    emumeanT[:, real_x.flatten()],
+                    emuvar[real_x, :, real_x.T],
+                    obs,
+                    is_cov,
+                )
+                acq_func.append(posteivar)
 
-    fig, ax = plt.subplots()
-    ax.plot(thetatest, ptest, color="black")
-    ax.plot(
-        thetatest.flatten(),
-        emumean_test.flatten(),
-        linestyle="dashed",
-        color="blue",
-        linewidth=2.5,
-    )
-    plt.fill_between(
-        thetatest.flatten(),
-        (emumean_test - np.sqrt(emumean_var)).flatten(),
-        (emumean_test + np.sqrt(emumean_var)).flatten(),
-        alpha=0.3,
-    )
-    ax.scatter(theta.T, ptr, color="red", s=60)
-    ax.set_xlabel(r"$\theta$", fontsize=ft)
-    ax.set_ylabel(r"$p(y|\theta)$", fontsize=ft)
-    ax.tick_params(axis="both", labelsize=ft)
-    plt.savefig("Figure2c.png", bbox_inches="tight")
+            return acq_func
+
+        acq = compute_eivar_full(clist, theta, thetatest, emu)
+        acqtr = compute_eivar_full(theta, theta, thetatest, emu)
+
+        ft = 20
+        # Figure 3 (a)/ 2nd row
+        cand_theta = clist[np.argmin(acq)]
+        fig, ax = plt.subplots()
+        ax.plot(clist, acq, color="blue")
+        ax.set_xlabel(r"$\theta$", fontsize=ft)
+        ax.set_ylabel("EIVAR", fontsize=16)
+        ax.tick_params(axis="both", labelsize=ft)
+        plt.scatter(cand_theta, np.min(acq), color="green", marker="*", s=200)
+        plt.scatter(theta, acqtr, color="red", marker="o", s=60)
+        plt.savefig("Figure3_1_" + str(i) + ".png", bbox_inches="tight")
+        # plt.show()
+
+        fig, ax = plt.subplots()
+        ax.plot(thetatest, ptest, color="black")
+        ax.plot(thetatest, posttesthat, color="blue", linestyle="dashed", linewidth=2.5)
+        plt.fill_between(
+            thetatest.flatten(),
+            (posttesthat - np.sqrt(posttestvar)).flatten(),
+            (posttesthat + np.sqrt(posttestvar)).flatten(),
+            alpha=0.3,
+        )
+
+        postcand, postcandvar = post.predict(cand_theta.reshape(1, 1))
+        plt.scatter(cand_theta, postcand, color="green", marker="*", s=200)
+        ax.scatter(theta.T, posttrhat, color="red", s=60)
+        ax.set_xlabel(r"$\theta$", fontsize=ft)
+        ax.set_ylabel(r"$p(y|\theta)$", fontsize=ft)
+        ax.tick_params(axis="both", labelsize=ft)
+        plt.savefig("Figure3_2_" + str(i) + ".png", bbox_inches="tight")
+        # plt.show()
+
+        theta = np.concatenate((theta, cand_theta.reshape(1, 1)))
+        f = np.concatenate((f, cls_sinlin.function(cand_theta.reshape(1, 1))))
