8469f051f9729de534c6b2cc058426b330d5622b
Ozge Surer <54813137+ozgesurer@users.noreply.github.com>
2024-10-05 22:08:48 -0400

    Update utils.py

--- PUQ/designmethods/utils.py
+++ PUQ/designmethods/utils.py
@@ -38,5 +38,63 @@ def parse_arguments():
         type=str,
         help="Name of the function.",
     )
+    parser.add_argument(
+        "-candsize", metavar="N2", default=100, type=int, help="Candidate size."
+    )
+    parser.add_argument(
+        "-refsize", metavar="N2", default=100, type=int, help="Reference list size."
+    )
+    parser.add_argument(
+        "-believer", metavar="N2", default=0, type=int, help="Kriging believer type."
+    )
     args = parser.parse_args()
     return args
+
+
+def save_output(desing_obj, name, al_func, nworker, minibatch, seedno):
+    outputname = (
+        "output_" + name + "_" + al_func + "_w_" + str(nworker) + "_b_" + str(minibatch)
+    )
+    if not os.path.isdir(outputname):
+        os.mkdir(outputname)
+
+    design_path = (
+        outputname
+        + "/"
+        + name
+        + "_"
+        + al_func
+        + "_w_"
+        + str(nworker)
+        + "_b_"
+        + str(minibatch)
+        + "_seed_"
+        + str(seedno)
+        + ".pkl"
+    )
+    with open(design_path, "wb") as file:
+        pickle.dump(desing_obj, file)
+
+
+def read_output(path1, name, al_func, nworker, minibatch, seedno):
+    outputname = "output"  # + name + '_' + al_func + '_w_' + str(nworker) + '_b_' + str(minibatch)
+    design_path = (
+        path1
+        + outputname
+        + "/"
+        + name
+        + "_"
+        + al_func
+        + "_w_"
+        + str(nworker)
+        + "_b_"
+        + str(minibatch)
+        + "_seed_"
+        + str(seedno)
+        + ".pkl"
+    )
+    with open(design_path, "rb") as file:
+        design_obj = pickle.load(file)
+
+    return design_obj
+
