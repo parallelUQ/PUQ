22125682ac68cfdc8efcd7d00aa08a9f197e8912
ozgesurer <ozgesurer2019@u.northwestern.edu>
2025-09-13 23:19:08 -0400

    Including Techno2024 paper with homGP

---
+++ examples/Technometrics2024/figure1b_new.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as sps
+from PUQ.designmethods.utils import parse_arguments
+from PUQ.prior import prior_dist
+from PUQ.designmethods.sequential_deterministic import sequential_design
+from smt.sampling_methods import LHS
+from test_funcs_new import unimodal
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    cls_unimodal = unimodal()
+
+    # # # Create a mesh for test set # # #
+    xpl = np.linspace(
+        cls_unimodal.thetalimits[0][0], cls_unimodal.thetalimits[0][1], 50
+    )
+    ypl = np.linspace(
+        cls_unimodal.thetalimits[1][0], cls_unimodal.thetalimits[1][1], 50
+    )
+    Xpl, Ypl = np.meshgrid(xpl, ypl)
+    thetatest = np.vstack([Xpl.ravel(), Ypl.ravel()]).T
+    ftest = np.zeros((thetatest.shape[0], 1))
+    for i in range(thetatest.shape[0]):
+        ftest[i, 0] = cls_unimodal.function(thetatest[i,0], thetatest[i,1])
+
+    ptest = sps.norm.pdf(
+        cls_unimodal.real_data - ftest, 0, np.sqrt(cls_unimodal.obsvar)
+    )
+
+    test_data = {"theta": thetatest, "f": ftest, "p": ptest, "p_prior": 1}
+    # # # # # # # # # # # # # # # # # # # # #
+    prior_func = prior_dist(dist="uniform")(
+        a=cls_unimodal.thetalimits[:, 0], b=cls_unimodal.thetalimits[:, 1]
+    )
+    
+    # Initial sample
+    n0 = 10
+    s = 1
+    sampling = LHS(xlimits=cls_unimodal.thetalimits, random_state=int(s))
+    t0 = sampling(n0)
+    f0 = np.zeros((t0.shape[0], 1))
+    for i in range(t0.shape[0]):
+        f0[i, 0] = cls_unimodal.function(t0[i,0], t0[i,1])
+
+    for af in ["rnd", "var", "ivar"]:
+        des_obj = sequential_design(cls_unimodal)
+        des_obj.build_design(z0=t0, 
+                             f0=f0, 
+                             T=50, 
+                             test=test_data, 
+                             af=af,
+                             args={
+                                    "mini_batch": 1,
+                                    "n_init_thetas": 10,
+                                    "nworkers": 2,
+                                    "seed_n0": 1,
+                                    "prior": prior_func,
+                                    "data_test": test_data,
+                                    "max_evals": 60,
+                                    "type_init": None,
+                                    "seed": 0,
+                                    "integral": "LHS"
+                                })
+    
+    
+        theta_al = des_obj.zs
+    
+        fig, ax = plt.subplots()
+        cp = ax.contour(Xpl, Ypl, ptest.reshape(50, 50), 20, cmap="RdGy")
+        ax.scatter(theta_al[n0:, 0], theta_al[n0:, 1], c="black", marker="+", zorder=2)
+        ax.scatter(
+            theta_al[0:n0, 0],
+            theta_al[0:n0, 1],
+            zorder=2,
+            marker="o",
+            facecolors="none",
+            edgecolors="blue",
+        )
+        ax.set_xlabel(r"$\theta_1$", fontsize=16)
+        ax.set_ylabel(r"$\theta_2$", fontsize=16)
+        ax.tick_params(axis="both", labelsize=16)
+        plt.savefig("Figure1b.png", bbox_inches="tight")
---
+++ examples/Technometrics2024/test_funcs_new.py
@@ -0,0 +1,109 @@
+import numpy as np
+import scipy.stats as sps
+
+
+class unimodal:
+    def __init__(self):
+        self.data_name = "unimodal"
+        self.thetalimits = np.array([[-4, 4], [-4, 4]])
+        self.obsvar = np.array([[4]], dtype="float64")
+        self.real_data = np.array([[-6]], dtype="float64")
+        self.out = [("f", float)]
+        self.d = 1
+        self.p = 2
+        self.x = np.arange(0, self.d)[:, None]
+        self.real_x = np.arange(0, self.d)[:, None]
+        ####
+        self.dx = 1
+        self.dt = 1
+
+    def function(self, theta1, theta2):
+        """
+        Wraps the unimodal function
+        """
+        thetas = np.array([theta1, theta2]).reshape((1, 2))
+        S = np.array([[1, 0.5], [0.5, 1]])
+        f = (thetas @ S) @ thetas.T
+        return f
+
+
+
+class banana:
+    def __init__(self):
+        self.data_name = "banana"
+        self.thetalimits = np.array([[-20, 20], [-10, 5]])
+        self.obsvar = np.array([[10**2, 0], [0, 1]])
+        self.real_data = np.array([[1, 3]], dtype="float64")
+        self.out = [("f", float, (2,))]
+        self.p = 2
+        self.d = 2
+        self.x = np.arange(0, self.d)[:, None]
+        self.real_x = np.arange(0, self.d)[:, None]
+        ####
+        self.dx = 1
+        self.dt = 1
+
+    def function(self, theta1, theta2):
+        f = np.array([theta1, theta2 + 0.03 * theta1**2])
+        return f
+
+
+
+class unidentifiable:
+    def __init__(self):
+        self.data_name = "unidentifiable"
+        self.thetalimits = np.array([[-8, 8], [-8, 8]])
+        self.obsvar = np.array([[1 / 0.01, 0], [0, 1]])
+        self.real_data = np.array([[0, 0]], dtype="float64")
+        self.out = [("f", float, (2,))]
+        self.d = 2
+        self.p = 2
+        self.x = np.arange(0, self.d)[:, None]
+        self.real_x = np.arange(0, self.d)[:, None]
+        ####
+        self.dx = 1
+        self.dt = 1
+
+    def function(self, theta1, theta2):
+        f = np.array([theta1, theta2])
+        return f
+
+
+
+class bimodal:
+    def __init__(self):
+        self.data_name = "bimodal"
+        self.thetalimits = np.array([[-6, 6], [-4, 8]])
+        self.obsvar = np.array([[1 / np.sqrt(0.2), 0], [0, 1 / np.sqrt(0.75)]])
+        self.real_data = np.array([[0, 2]], dtype="float64")
+        self.out = [("f", float, (2,))]
+        self.d = 2
+        self.p = 2
+        self.x = np.arange(0, self.d)[:, None]
+        self.real_x = np.arange(0, self.d)[:, None]
+        ####
+        self.dx = 1
+        self.dt = 1
+
+    def function(self, theta1, theta2):
+        f = np.array([theta2 - theta1**2, theta2 - theta1])
+        return f
+
+
+
+def create_test_data(al_test, cls_func):
+    ftest = al_test._info["f"]
+    thetatest = al_test._info["theta"]
+    ptest = np.zeros(thetatest.shape[0])
+
+    if cls_func.data_name == "unimodal":
+        ptest = sps.norm.pdf(cls_func.real_data - ftest, 0, np.sqrt(cls_func.obsvar))
+    else:
+        for i in range(ftest.shape[0]):
+            mean = ftest[i, :]
+            rnd = sps.multivariate_normal(mean=mean, cov=cls_func.obsvar)
+            ptest[i] = rnd.pdf(cls_func.real_data)
+
+    test_data = {"theta": thetatest, "f": ftest, "p": ptest, "p_prior": 1}
+
+    return test_data
